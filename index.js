c4eb66d910fb2ff946add8f20340e8ef8abc2cb3
JingYan Wang <jingyan.wang@yuneec.com>
2021-06-02 10:34:44 +0800

    Add file_name

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const exec = require("@actions/exec");
 const toolCache = require("@actions/tool-cache");
 const path = require("path");
@@ -11,6 +10,7 @@ const url = "http://gosspublic.alicdn.com/ossutil/1.6.7/ossutil64";
 
 async function main() {
     var file_path = core.getInput('args');
+    var file_name = path.basename(file_path);
     var ota_server_url = core.getInput('ota_server_url');
     var ota_server_user = core.getInput('ota_server_user');
     var ota_server_pwd = core.getInput('ota_server_pwd');
@@ -52,16 +52,34 @@ async function main() {
         file_path,
         aliyun_oss_url
     ]);
-    console.log(exitCode);
     if(exitCode != 0) {
         core.setFailed("ossutil upload Failed");
         return;
     }
+    core.info(`exitCode: ${exitCode}`);
+    let myOutput = '';
+    let myError = '';
+    const options = {};
+    options.listeners = {
+    stdout: (data: Buffer) => {
+        myOutput += data.toString();
+    },
+    stderr: (data: Buffer) => {
+        myError += data.toString();
+    }
+    };
+    await exec.exec("ossutil", [
+        "sign",
+        aliyun_oss_url + file_name,
+        "--disable-encode-slash"
+    ], options);
+    core.info(myOutput);
+
     var array = file_path.match(/[0-9]+\.[0-9]+\.[0-9]+/g);
     var VERSION = array ? array[0] : "0.0.0"
     var MD5 = crypto.createHash('md5').update(fs.readFileSync(file_path), 'utf8').digest('hex');
     var JOB_ID = +new Date() / 1000;
-    var PATH_ = core.getInput('aliyun_oss_url') + path.basename(file_path);
+    var PATH_ = myOutput.split('?')[0];
 
     var RESULT = `{
         "Version": "${VERSION}",
@@ -72,7 +90,6 @@ async function main() {
         "Path2": "",
         "Path3": ""
     }`;
-    console.log(RESULT)
     var form_data = {
         headers: { 'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' },
         body: `'------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"username\"\r\n\r\n${ota_server_user}\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"password\"\r\n\r\n${ota_server_pwd}\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--'`
